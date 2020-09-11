9b631030b2df9dd5e0bbf0a25de8d592f42c8c6d
Yasir Zafar <yzyasir@yahoo.com>
2020-09-10 23:54:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// why do I have to define useState in curlies?
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const getNames = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    // .then(res => { console.log(res.json()), did this to show what shows in console
+    .then(response => response.json())
+// We have to turn the response into some sort of json by retrning
+// the above will show the data as a json objects
+// the response.all is shows the data as an array not an object, easier to parse the data
+    .then(response => {
+      console.log(response)
+      setPokemon(response.results)
+// this will now put the data into the state, for it to be displayed
+// when inspecting look inside the components
+    })
+// .then is always to resolve, res is short for response
+
+    .catch(err => console.log(err))
+// .catch is always to reject/error
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button className="btn btn-primary" onClick={getNames}>Click here for ALL Pokemon names</button>
+{/* When you hit the button onClick will activate its trap card getNames */}
+      <ol>
+      {
+        
+        pokemon.map((pokem, i) => {
+// .map creates a new array with the results of calling a function for every array element, taking it out from the state
+              return <li> <p key={i} >{pokem.name}</p> </li>
+        })
+      }
+      </ol>
     </div>
   );
 }
